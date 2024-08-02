8366aebe16e303d4bc10f773142b62c79aa82fef
VitorGreff <vitorgreffdias@gmail.com>
2024-08-02 19:43:33 -0300

    simplifying autoformat

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>w", ":w!<CR>", { desc = "Force write" })
+vim.keymap.set("n", "<leader>w", function()
+  vim.lsp.buf.format() -- Format the buffer
+  vim.cmd("w!") -- Force write
+end, { desc = "Force write and format" })
 vim.keymap.set("n", "<leader>q", ":q!<CR>", { desc = "Force quit" })
 
